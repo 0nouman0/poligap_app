0bfdcf01d8e292db01fb061ad53b32af59102b29
User <136671080+0nouman0@users.noreply.github.com>
2025-10-16 12:16:25 +0530

    theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange={false}
       storageKey="poligap-theme"
